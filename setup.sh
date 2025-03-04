20cca7198c4338fae5546a9fb5deb3c575d478fd
SamWang8891 <g348.8891@gmail.com>
2025-03-04 14:43:30 +0800

    Fix: Use bash, fix reset password and some other things

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 
 # Warn about the root directory naming
-if [ ${PWD##*/} != "simple-url-shortener" ]; then
-    if [ ${PWD##*/} == "simple-url-shortener-modding" ]; then
+if [ "${PWD##*/}" != "simple-url-shortener" ]; then
+    if [ "${PWD##*/}" == "simple-url-shortener-modding" ]; then
         echo -e "Warning: You are now using modding branch. Switch to main branch if you are not developing.\n"
         echo -e "You'll need to build the files yourself according to READNE.md\n"
     else
@@ -10,12 +11,13 @@ if [ ${PWD##*/} != "simple-url-shortener" ]; then
         echo -e "\nPlease stay in the simple-url-shortener directory(mandatory) or rename the root directory to \"simple-url-shortener\"(suggestion).\n"
     fi
 
-    read -p "Do you want to continue (yes/no): " confirm
-    if [ $confirm == "no" ]; then
+    read -r -p "Do you want to continue (yes/no): " confirm
+    if [ "$confirm" == "no" ]; then
         echo -e "Exiting...\n"
         exit 1
     fi
 fi
+
 
 # Check current directory
 if [ ! -f "docker/web/index.html" ] || [ ! -f "docker/backend/app.py" ]; then
@@ -27,10 +29,13 @@ fi
 # Prompt if user is here to reset password
 if [ "$(docker ps -aq -f name=simple-url-shortener)" ]; then
   echo ""
-  read -p "Are you here for resetting admin password (yes/no): " confirm
-  if [ $confirm == "yes" ]; then
+  read -r -p "Are you here for resetting admin password (yes/no): " confirm
+  if [ "$confirm" == "yes" ]; then
     echo "1" > docker/backend/is_reset_password.txt
-    echo "Password reset!"
+    echo -e "Password reset!\n"
+    echo -e "Restarting the service, please wait patiently...\n"
+    docker restart simple-url-shortener-nginx
+    docker restart simple-url-shortener-python
     echo -e "Exiting...\n"
     exit 0
   fi
@@ -49,12 +54,11 @@ fi
 # Check if existing service is already installed on docker
 if [ "$(docker ps -aq -f name=simple-url-shortener)" ]; then
     echo ""
-    read -p "There is an existing one, do you want to reinstall and reconfigure it? (yes/no): " confirm
-    if [ $confirm == "yes" ]; then
+    read -r -p "There is an existing one, do you want to reinstall and reconfigure it? (yes/no): " confirm
+    if [ "$confirm" == "yes" ]; then
         echo -e "\nStopping, please wait patiently..."
         docker stop simple-url-shortener-nginx
         docker stop simple-url-shortener-python
-        docker-compose down
         docker rm simple-url-shortener-nginx
         docker rm simple-url-shortener-python
     else
@@ -86,7 +90,7 @@ fi
 echo -e "\nPlease enter the base URI that is used to show on the webpage of the generated link (including https:// or http:// and the port number behind if not 80 or 443)."
 echo -e "If you are routing this through a proxy, you should enter the proxied information.\n"
 echo -e "Please note that the default port of is 8887, if you are using a different port, you should change it in the docker-compose.yml file.\n"
-read -p "Please enter: " baseurl
+read -r -p "Please enter: " baseurl
 echo "BASE_URL=$baseurl" > docker/backend/.env
 echo "hostname: \"$baseurl\"" > docker/web/conf.yaml
 
